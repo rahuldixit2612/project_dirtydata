83083f5533846833a120e0983a505e88000e10f3
rahuldixit2612 <rahuldixit0716@gmail.com>
2022-07-10 02:19:05 +0530

    First Initial Commit

---
+++ dirty_data.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sn
+import sklearn as sk
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+# reading file using pandas
+df = pd.read_csv('dirtydata.csv')
+
+
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+# read first five values
+df.head()
+
+
+# In[5]:
+
+
+## read last five values
+df.tail()
+
+
+# In[6]:
+
+
+# checking columns
+df.columns
+
+
+# In[7]:
+
+
+# for complete information about data or table
+df.info()
+
+
+# In[8]:
+
+
+#checking for null values 
+df.isnull().sum()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
